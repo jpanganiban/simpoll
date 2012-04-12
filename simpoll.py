f3b03a53f0a6878e9e9e9c80079447b35639a556
Jesse Panganiban <me@jpanganiban.com>
2012-04-12 20:04:01 +0800

    Added template

--- simpoll.py
+++ simpoll.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='views')
 
 
 @app.route('/')
 def index():
-  return 'Simpoll'
+  return render_template('index.html')
 
 
 def start():
