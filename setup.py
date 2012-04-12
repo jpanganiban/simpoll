7bdcc46be0829f85e1662e75f64bfc70ef9a3284
Jesse Panganiban <me@jpanganiban.com>
2012-04-12 19:56:28 +0800

    Added setup and readme

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(name='Simpoll',
+      version='0.1',
+      description='A long-polling demonstration with backbone.js',
+      author='Jesse Panganiban',
+      author_email='me@jpanganiban.com',
+      py_modules=['simpoll'],
+      entry_points={
+          'console_scripts': ['simpoll = simpoll:start']
+        },
+      install_requires=[
+          'flask'
+        ]
+      )
